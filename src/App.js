1f8958f6b96ace4d237e8e21d28886a9ad3fe1c3
sajjad-jbr <job.sajjad.jbr@gmail.com>
2022-10-30 15:05:00 +0330

    add blog component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './assets/styles/App.css';
 import Header from "./components/Header";
 import Content from "./components/Content";
-
+import {useSelector} from "react-redux";
+import {selectSignedIn} from "./features/userSlice";
+import Blogs from "./components/Blogs";
+import './assets/styles/App.css';
 function App() {
+    const isSignedIn = useSelector(selectSignedIn);
+
     return (
         <div className="App">
             <Header/>
             <Content/>
+            {isSignedIn && <Blogs/>}
         </div>
     );
 }
