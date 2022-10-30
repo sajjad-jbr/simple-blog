525bd77056ea1ad5baa5186907cfa07e5289a60f
sajjad-jbr <job.sajjad.jbr@gmail.com>
2022-10-30 13:48:45 +0330

    add header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import './assets/styles/App.css';
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          hello world !!!
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
